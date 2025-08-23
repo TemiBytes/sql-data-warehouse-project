2a8b0b22e29c0eb9af4445c79871ebab9d435506
Temidayo <temidayoakin.pro@gmail.com>
2025-08-23 04:03:39 +0100

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -6,7 +6,8 @@ DDL SCRIPT: Create Silver Tables
 Script Purpose:
   This script creates tables in the 'silver' schema, dropping existing tables
   if they already exist.
-  Run this script to redefine the DDL Structure of 'silver' Tables 
+  Run this script to redefine the DDL Structure of 'bronze' Tables 
+======================================================
 */
 
 IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
